fea8f215f10df8a2230b12eab46c81a3e7085ef3
AaronCDee <101570796+AaronCDee@users.noreply.github.com>
2025-02-08 10:53:21 +0200

    refactor: add html and fetching (#4)
    * refactor: add html and fetching
    
    * fix: deploys
    
    * refactor: remove counter
    
    * fix: error message
    
    ---------

---
+++ src/js/ArticlesListing.mjs
@@ -0,0 +1,50 @@
+import ExternalService from "./ExternalService";
+
+export default class ArticlesListing {
+  async init() {
+    const newsService = new ExternalService
+    let articles      = []
+
+    try {
+      articles = await newsService.getTopHeadlines()
+    } catch (error) {
+      console.error(error) // eslint-disable-line no-console
+      this.renderError(error.message)
+
+      return
+    }
+
+    this.renderArticles(this.filterArticles(articles))
+  }
+
+  filterArticles(articles) {
+    return articles.filter(article => article.content !== null && article.content !== "")
+  }
+
+  template(article) {
+    return `
+      <div class="article-card">
+        <h1 class="headline text">${article.title}</h1>
+        <p class="date">
+          Published on February 8, 2025 by ${article.author === null || article.author === "" ? "Anonymous" : article.author}
+        </p>
+        <p class="content text">${article.description}</p>
+        <a href="${article.url}" target="_blank" class="read-more">Read More →</a>
+      </div>
+    `
+  }
+
+  renderArticles(articles) {
+    const articlesHTML = articles.map(article => this.template(article)).join("")
+
+    this.container().insertAdjacentHTML("afterbegin", articlesHTML)
+  }
+
+  container() {
+    return document.querySelector(".articles-container")
+  }
+
+  renderError(error) {
+    this.container().insertAdjacentHTML("afterbegin", `There was an error loading articles: ${error}`)
+  }
+}
